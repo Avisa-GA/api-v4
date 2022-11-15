16685f0afe91f63a215f6cc67f5db785778c7610
Avisa Poshtkouhi <aap571420@gmail.com>
2022-11-14 16:36:37 -0800

    feat: ready for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from "dotenv";
 dotenv.config()
+import config from "./config"
 import app from "./server"
 const PORT = 3001
 
-app.listen(PORT, () => {
+app.listen(config.port, () => {
   console.log('🎧 🌺 server on http://localhost:3001')
 })
